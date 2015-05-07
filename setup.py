f99e3c49fffcc68845c8481d8cc58a0fb7470552
judasnow <judasnow@gmail.com>
2015-05-07 11:27:31 +0800

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
+
+setup(
+    name="weixin-helper",
+    version="0.1",
+    keywords=("weixin"),
+    description="",
+    license="BSD",
+    author="judasnow",
+    author_email="judasnow@gmail.com",
+    url="",
+    platforms="",
+    packages=find_packages(),
+    package_dir={}
+)
+
+
---
+++ weixin_helper/weixin_mp.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+import requests
+
+
+class AccessTokenError(Exception):
+    pass
+
+
+class WeixinMpHelper(object):
+    """weixin 公众号常用操作"""
+
+    def __init__(self, appid="", secure=""):
+        pass
+
+    def get_access_token(self):
+        pass
+
+    def get_jsapi_ticket(self):
+        pass
+
+    def get_js_config(self, apis=[]):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
